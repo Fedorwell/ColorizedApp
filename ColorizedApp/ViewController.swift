781edb5b85771e52a7ada73e9d4595454199f346
Fedorwell <115953771+Fedorwell@users.noreply.github.com>
2023-05-01 00:59:13 +0300

    Final version HW colorizedApp

--- ColorizedApp/ViewController.swift
+++ ColorizedApp/ViewController.swift
@@ -4,16 +4,70 @@
 //
 //  Created by mac on 28.04.2023.
 //
+//ColorizedApp
+//
+
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet var colorView: UIView!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupRgbSlider()
+        changeColor()
+        rgbSliderChanged(self)
+        
+        colorView.layer.cornerRadius = 12
+        colorView.layer.masksToBounds = true
+        
     }
-
-
+    
+    func changeColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1 )
+    }
+    
+    @IBAction func rgbSliderChanged(_ sender: Any) {
+        changeColor()
+        let valueRed = redSlider.value
+        let valueGreen = greenSlider.value
+        let valueBlue = blueSlider.value
+        
+        redLabel.text = String(format: "%.2f", valueRed)
+        greenLabel.text = String(format: "%.2f", valueGreen)
+        blueLabel.text = String(format: "%.2f", valueBlue)
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupRgbSlider(){
+        redSlider.value = 0.5
+        redSlider.minimumValue = 0
+        redSlider.maximumValue = 1
+        
+        greenSlider.value = 0.5
+        greenSlider.minimumValue = 0
+        greenSlider.maximumValue = 1
+        
+        blueSlider.value = 0.5
+        blueSlider.minimumValue = 0
+        blueSlider.maximumValue = 1
+        
+        changeColor()
+        
+    }
+    
 }
+
+
+
 
